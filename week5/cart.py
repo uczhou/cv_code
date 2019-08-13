3ba617e308e446d9f57ecb6ed407ab5c9fcc60d6
uczhou <hongleizhou@uchicago.edu>
2019-08-13 00:08:32 -0500

    to be debugged

---
+++ week5/cart.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+class Node:
+
+    def __init__(self, x, y, idxs, min_leaf=1):
+        self.x = x
+        self.y = y
+        self.idxs = idxs
+
+        self.min_leaf = min_leaf
+
+        self.row_count = len(idxs)
+        self.col_count = x.shape[1]
+        self.val = np.mean(y[idxs])
+        self.score = float('inf')
+        self.find_var_split()
+
+    def find_var_split(self):
+        for c in range(self.col_count):
+            self.get_split(c)
+        if self.is_leaf:
+            return
+        x = self.split_col
+        lhs = np.nonzero(x <= self.split)[0]
+        rhs = np.nonzero(x > self.split)[0]
+        self.lhs = Node(self.x, self.y, self.idxs[lhs], self.min_leaf)
+        self.rhs = Node(self.x, self.y, self.idxs[rhs], self.min_leaf)
+
+    def get_split(self, var_idx):
+        x = self.x.values[self.idxs, var_idx]
+
+        for r in range(self.row_count):
+            lhs = x <= x[r]
+            rhs = x > x[r]
+            if rhs.sum() < self.min_leaf or lhs.sum() < self.min_leaf:
+                continue
+
+            curr_score = self.find_score(lhs, rhs)
+            if curr_score < self.score:
+                self.var_idx = var_idx
+                self.score = curr_score
+                self.split = x[r]
+
+    def find_score(self, lhs, rhs):
+        y = self.y[self.idxs]
+        lhs_std = y[lhs].std()
+        rhs_std = y[rhs].std()
+        return lhs_std * lhs.sum() + rhs_std * rhs.sum()
+
+    @property
+    def split_col(self):
+        return self.x.values[self.idxs, self.var_idx]
+
+    @property
+    def is_leaf(self):
+        return self.score == float('inf')
+
+    def predict(self, x):
+        return np.array([self.predict_row(xi) for xi in x])
+
+    def predict_row(self, xi):
+        if self.is_leaf:
+            return self.val
+
+        node = self.lhs if xi[self.var_idx] <= self.split else self.rhs
+
+        return node.predict_row(xi)
+
+
+
+
+
+
---
+++ week5/id3.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+
+
+class Node:
+
+    def __init__(self, key, val, result, children):
+        self.key = key
+        self.val = val
+        self.result = result
+        self.children = children
+
+
+class ID3:
+
+    def __init__(self, data_set):
+        self.data_set = data_set
+        self.root = self.data_split(data_set, [], None, None)
+
+    @staticmethod
+    def calc_entropy(num, total):
+        # -p * logp
+        p = num / total
+        return -p * np.log(p)
+
+    def calc_info_gain(self, entropy, data_set, col_name):
+        group_count = data_set.groupby(col_name).size().reset_index(name='counts')
+        sub_group_count = data_set.groupby([col_name, 'label']).size().reset_index(name='counts')
+
+        total_data = data_set.shape[0]
+
+        col_entropy = 0
+        for i in range(group_count.shape[0]):
+            pos = sub_group_count.loc[
+                sub_group_count[col_name] == group_count.iloc[i][col_name] & sub_group_count['label'] == 1]['counts']
+            neg = sub_group_count.loc[
+                sub_group_count[col_name] == group_count.iloc[i][col_name] & sub_group_count['label'] == 0]['counts']
+            total = sub_group_count.iloc[i]['counts']
+            group_entropy = 0
+            if pos > 0:
+                group_entropy += self.calc_entropy(pos, total)
+
+            if neg > 0:
+                group_entropy += self.calc_entropy(neg, total)
+
+            col_entropy += (total / total_data) * group_entropy
+
+        return entropy - col_entropy
+
+    def attribute_selection(self, data_set, cols):
+        # calculate entropy of data_set
+        mask_ones = data_set['label'] == 1
+        mask_zeros = data_set['label'] == 0
+
+        entropy = self.calc_entropy(len(mask_ones), data_set.shape[0]) + self.calc_entropy(len(mask_zeros), data_set.shape[0])
+
+        # calculate information gain
+        info_gains = {}
+        for col in data_set.columns:
+            if col not in cols:
+                info_gains = {col: self.calc_info_gain(entropy, data_set[[col, 'label']], col)}
+
+        sorted_info_gains = sorted(info_gains.keys(), key=lambda x: -x[1])
+
+        # get the most information gain
+        return sorted_info_gains[0][0]
+
+    def data_split(self, data_set, cols, key, val):
+        if len(cols) == data_set.shape[1] - 1:
+            return None
+
+        if len(cols) == 0:
+            root = Node(key, val, None, [])
+
+            col = self.attribute_selection(data_set, cols)
+            cols.append(col)
+
+            groups = data_set[col].drop_duplicates().values.tolist()
+            for group in groups:
+                mask = data_set[col] == group
+                if mask.size > 0:
+                    child = self.data_split(data_set[mask], cols, col, group)
+                    if child:
+                        root.children.append(child)
+
+            return root
+
+        node = Node(key, val, None, [])
+
+        mask_ones = data_set['label'] == 1
+        mask_zeros = data_set['label'] == 0
+
+        col = self.attribute_selection(data_set, cols)
+        cols.append(col)
+
+        if len(mask_ones) == data_set.shape[0]:
+            node.result = True
+            return node
+        elif len(mask_zeros) == data_set.shape[0]:
+            node.result = False
+            return node
+
+        groups = data_set[col].drop_duplicates().values.tolist()
+        for group in groups:
+            mask = data_set[col] == group
+            if mask.size > 0:
+                child = self.data_split(data_set[mask], cols, col, group)
+                if child:
+                    node.children.append(child)
+
+        return node
+
+    def predict(self, node, data):
+
+        for child in node.children:
+            if data[child.val] == data[child.key]:
+                if child.result:
+                    return child.result
+                else:
+                    return self.predict(child, data)
+
+        return False
+
+
+
