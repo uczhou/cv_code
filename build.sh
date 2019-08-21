31dcf2e0e6594fa31fb528c7ba7e673cab191994
uczhou <hongleizhou@uchicago.edu>
2019-08-21 01:34:12 -0500

    commit

---
+++ build.sh
@@ -0,0 +1,62 @@
+echo "OpenCV installation by learnOpenCV.com"
+ 
+#Specify OpenCV version
+cvVersion="3.4.4"
+
+# Clean build directories
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+
+# Create directory for installation
+mkdir installation
+mkdir installation/OpenCV-"$cvVersion"
+
+# Save current working directory
+cwd=$(pwd)
+
+sudo apt -y update
+sudo apt -y upgrade
+ 
+## Install dependencies
+sudo apt-get -y install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install python3.5-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get -y install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout $cvVersion
+cd ..
+ 
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout $cvVersion
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D BUILD_TIFF=ON \
+        -D WITH_CUDA=OFF \
+        -D ENABLE_AVX=OFF \
+        -D WITH_OPENGL=OFF \
+        -D WITH_OPENCL=OFF \
+        -D WITH_IPP=OFF \
+        -D WITH_TBB=ON \
+        -D BUILD_TBB=ON \
+        -D WITH_EIGEN=OFF \
+        -D WITH_V4L=OFF \
+        -D WITH_VTK=OFF \
+        -D BUILD_TESTS=OFF \
+        -D BUILD_PERF_TESTS=OFF \
+        -D CMAKE_BUILD_TYPE=RELEASE \
+        -D CMAKE_INSTALL_PREFIX=/usr/local \
+        -D OPENCV_EXTRA_MODULES_PATH=$cwd/opencv_contrib/modules $cwd/opencv/
+
+make -j4
+make install
+
+sudo ldconfig -v
+
+pkg-config --modversion opencv
